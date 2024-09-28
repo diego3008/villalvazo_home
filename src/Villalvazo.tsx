f6be5e3e6d7eb40de4c73e39d33e5bafa0d15be7
Diego Rubalcava <diegoarlechuga@gmail.com>
2024-09-28 15:19:44 -0600

    started project

---
+++ src/Villalvazo.tsx
@@ -0,0 +1,7 @@
+export const Villalvazo = () => {
+    return (
+        <div className="flex justify-center mx-auto">
+            <h1 className="text-red-500 text-lg">Hola</h1>
+        </div>
+    );
+};
